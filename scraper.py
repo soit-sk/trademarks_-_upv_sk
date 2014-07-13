442b8a33a95f93421aa33cc312441f72387fe9d7
Lubomir Rintel <lkundrak@v3.sk>
2014-07-13 14:54:59 +0200

    Save min_id before we try fetching an entry
    Otherwise the min_id would not get saved for failed or missing entries.

--- scraper.py
+++ scraper.py
@@ -75,6 +75,7 @@ print "Start or continue from id: ", min_id
 max_id = getMaxId()
 
 for id in xrange(min_id, max_id+1):
+    scraperwiki.sqlite.save_var("min_id", id)
     try:
         root = fetchHtml(detailUrl % id)
     except:
@@ -101,6 +102,5 @@ for id in xrange(min_id, max_id+1):
     
     if len(dbData) > 1: #skip page in case no data for id is return
         scraperwiki.sqlite.save(unique_keys=['id'], data = dbData)
-    scraperwiki.sqlite.save_var("min_id", id+1)
 
 scraperwiki.sqlite.save_var("min_id", 1)
